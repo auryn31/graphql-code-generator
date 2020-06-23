9cbd677139618766eecd19bffcc86ab0f6a403ca
Auryn Engel <auryn.engel@sap.com>
2020-06-23 12:11:19 +0200

    dont generate fragments because they already come from typescript-react-apollo

--- packages/plugins/typescript/react-apollo-offix/src/index.ts
+++ packages/plugins/typescript/react-apollo-offix/src/index.ts
@@ -1,29 +1,20 @@
 import { Types, PluginValidateFn, PluginFunction } from '@graphql-codegen/plugin-helpers';
 import { visit, GraphQLSchema, concatAST, Kind, FragmentDefinitionNode } from 'graphql';
-import {
-  LoadedFragment,
-  RawClientSideBasePluginConfig
-} from '@graphql-codegen/visitor-plugin-common';
+import { LoadedFragment, RawClientSideBasePluginConfig } from '@graphql-codegen/visitor-plugin-common';
 import { ReactApolloVisitor } from './visitor';
 import { extname } from 'path';
 
-export const plugin: PluginFunction<RawClientSideBasePluginConfig> = (
-  schema: GraphQLSchema,
-  documents: Types.DocumentFile[],
-  config: RawClientSideBasePluginConfig
-) => {
+export const plugin: PluginFunction<RawClientSideBasePluginConfig> = (schema: GraphQLSchema, documents: Types.DocumentFile[], config: RawClientSideBasePluginConfig) => {
   const allAst = concatAST(documents.map(v => v.document));
 
   const allFragments: LoadedFragment[] = [
-    ...(allAst.definitions.filter(
-      d => d.kind === Kind.FRAGMENT_DEFINITION
-    ) as FragmentDefinitionNode[]).map(fragmentDef => ({
+    ...(allAst.definitions.filter(d => d.kind === Kind.FRAGMENT_DEFINITION) as FragmentDefinitionNode[]).map(fragmentDef => ({
       node: fragmentDef,
       name: fragmentDef.name.value,
       onType: fragmentDef.typeCondition.name.value,
-      isExternal: false
+      isExternal: false,
     })),
-    ...(config.externalFragments || [])
+    ...(config.externalFragments || []),
   ];
 
   const visitor = new ReactApolloVisitor(schema, allFragments, config, documents);
@@ -31,19 +22,11 @@ export const plugin: PluginFunction<RawClientSideBasePluginConfig> = (
 
   return {
     prepend: visitor.getImports(),
-    content: [
-      visitor.fragments,
-      ...visitorResult.definitions.filter(t => typeof t === 'string')
-    ].join('\n')
+    content: [...visitorResult.definitions.filter(t => typeof t === 'string')].join('\n'),
   };
 };
 
-export const validate: PluginValidateFn<any> = async (
-  _schema: GraphQLSchema,
-  _documents: Types.DocumentFile[],
-  _config: RawClientSideBasePluginConfig,
-  outputFile: string
-) => {
+export const validate: PluginValidateFn<any> = async (_schema: GraphQLSchema, _documents: Types.DocumentFile[], _config: RawClientSideBasePluginConfig, outputFile: string) => {
   if (extname(outputFile) !== '.tsx' && extname(outputFile) !== '.ts') {
     throw new Error(`Plugin "react-apollo" requires extension to be ".tsx" or ".ts!`);
   }
